3b37f459aff24aa1af0d6c3b604803a550c01953
Heindrick Cheung <hcheung01@gmail.com>
2018-11-09 17:20:01 +0000

    copy of base model and original

--- models/copy_base_model.py
+++ models/copy_base_model.py
@@ -2,34 +2,26 @@
 """
 class module
 """
-from datetime import datetime
-import uuid
+import models
+import uuid, datetime
+
 
 class BaseModel():
     """Base Class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initialization Method"""
-        if kwargs:
-            for key, val in kwargs.items():
-                if key == 'create_at' or key == 'updated_at':
-                    dt_time_obj = datetime.strftime(val, '%Y, %-m, %-d', )
-                setattr(self, key, val)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    # print the class
     def __str__(self):
         """print object"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    # update datetime
     def save(self):
         """update the public instance attribute"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """returns instance dictionary"""
