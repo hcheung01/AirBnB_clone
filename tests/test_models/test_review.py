20799eb608fff4ce822227391d6299aa8985b98d
Heindrick Cheung <hcheung01@gmail.com>
2018-11-15 00:44:18 +0000

    update review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -38,8 +38,18 @@ class TestReview(unittest.TestCase):
         Return:
             na
         """
+        self.a.save()
+        self.b.save()
         del self.a
         del self.b
+        myfile = "/home/vagrant/AirBnB_clone/file.json"
+        self.assertTrue(os.path.isfile(myfile))
+        try:
+            os.path.isfile(myfile)
+            os.remove(myfile)
+            self.assertFalse(os.path.isfile(myfile))
+        except FileNotFoundError:
+                pass
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8"""
@@ -134,20 +144,6 @@ class TestReview(unittest.TestCase):
         """
 
         self.assertTrue(issubclass(Review, BaseModel))
-
-    def test_save_json(self):
-        """test if inherit from basemodel and save to json"""
-
-        self.a.save()
-        self.b.save()
-        myfile = "/home/vagrant/AirBnB_clone/file.json"
-        self.assertTrue(os.path.isfile(myfile))
-        try:
-            os.path.isfile(myfile)
-            os.remove(myfile)
-            self.assertFalse(os.path.isfile(myfile))
-        except FileNotFoundError:
-            pass
 
     def test_file_exist(self):
         """test if file exist after delete"""
