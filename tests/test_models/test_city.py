558f4807fca1842eb499c8e00996b5972c615b45
Heindrick Cheung <hcheung01@gmail.com>
2018-11-15 00:32:34 +0000

    updated teardown

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -39,8 +39,18 @@ class TestCity(unittest.TestCase):
         Return:
             na
         """
+        self.a.save()
+        self.b.save()
         del self.a
         del self.b
+        myfile = "/home/vagrant/AirBnB_clone/file.json"
+        self.assertTrue(os.path.isfile(myfile))
+        try:
+            os.path.isfile(myfile)
+            os.remove(myfile)
+            self.assertFalse(os.path.isfile(myfile))
+        except FileNotFoundError:
+            pass
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8"""
@@ -128,25 +138,11 @@ class TestCity(unittest.TestCase):
 
         self.assertTrue(issubclass(City, BaseModel))
 
-    def test_save_json(self):
-        """test if inherit from basemodel and save to json"""
-
-        self.a.save()
-        self.b.save()
-        myfile = "/home/vagrant/AirBnB_clone/file.json"
-        self.assertTrue(os.path.isfile(myfile))
-        try:
-            os.path.isfile(myfile)
-            os.remove(myfile)
-            self.assertFalse(os.path.isfile(myfile))
-        except FileNotFoundError:
-            pass
-
     def test_file_exist(self):
         """test if file exist after delete"""
 
         myfile = "/home/vagrant/AirBnB_clone/file.json"
-        self.assertTrue(os.path.isfile(myfile))
+        self.assertFalse(os.path.isfile(myfile))
 
 if __name__ == "__main__":
     unittest.main()
